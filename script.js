ba2a84801b08e418c45bb6568b065b296f80e75e
Miarintsoa <23l04l03@gmail.com>
2025-02-12 14:59:42 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('download-btn').addEventListener('click', function () {
-    const link = document.createElement('a');
-    link.href = 'README.md'; // Remplace par le nom exact de ton fichier
-    link.download = 'README.md'; // Nom du fichier téléchargé
-    link.click();
-});
